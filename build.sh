cb6fe837eb1af3a26563000311a5350abae13a1d
Your Name <you@example.com>
2016-12-29 15:08:32 +0200

    init

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+REPOS=(
+    "bitbucket.attic.pw/scm/smar/abs.git"
+    "bitbucket.attic.pw/scm/smar/agent.git"
+    "bitbucket.attic.pw/scm/smar/backoffice.git"
+    "bitbucket.attic.pw/scm/smar/info.git"
+    "bitbucket.attic.pw/scm/smar/keyserver.git"
+    "bitbucket.attic.pw/scm/smar/merchant-bot.git"
+    "bitbucket.attic.pw/scm/smar/nginx-proxy.git"
+    "bitbucket.attic.pw/scm/smar/offline.git"
+    "bitbucket.attic.pw/scm/smar/web-wallet.git"
+    "bitbucket.attic.pw/scm/smar/welcome.git"
+    "bitbucket.attic.pw/scm/cryp/api.git"
+)
+
+GIT_USER=''
+GIT_PASS=''
+BASE_DIR='/vhosts/'
+
+while true
+do
+    read -ra key -p "Git login: "
+    if [[ $key == '' ]]; then
+        echo "Error: git login is empty. Try again."
+        continue
+    fi
+
+    GIT_USER=$key
+    break
+done
+
+while true
+do
+    stty_orig=`stty -g` # save original terminal setting.
+    stty -echo          # turn-off echoing.
+    read -ra key -p "Git password for $GIT_USER: "
+    stty $stty_orig     # restore terminal setting.
+
+    if [[ $key == '' ]]; then
+        echo "Error: git password is empty. Try again."
+        continue
+    fi
+
+    GIT_PASS=$key
+    break
+done
+
+for i in "${REPOS[@]}"
+do
+    dir=$(basename "$i" ".git")
+    dir=$BASE_DIR$dir
+    git clone -b nbu "http://$GIT_USER:$GIT_PASS@$i" $dir
+    cd $dir && make build && cd ..
+done
